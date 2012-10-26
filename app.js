8175fc1348d7e6cd6cec8804f204d10f2237cf03
Theoxylo <theoxylo@yahoo.com>
2012-10-26 15:16:35 -0400

    added upload form link to home page, form as ejs

--- app.js
+++ app.js
@@ -1,87 +1,96 @@
-var express = require('express')
-  , log = console.log
-  , fs = require('fs')
-  , gdsn = require('gdsn')
-  ;
+var express = require('express'),
+    log = console.log,
+    fs = require('fs'),
+    gdsn = require('gdsn');
 
 var inboxDir = __dirname + '/msg/inbox/';
 var outboxDir = __dirname + '/msg/outbox/';
 var app = express();
 
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  //app.set('views', __dirname + '/views');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+    app.set('port', process.env.PORT || 3000);
+    //app.set('views', __dirname + '/views');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function() {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
 
 // CIN upload form
 app.get('/cin', function(req, res) {
-  res.send('<p>Please select the inbound CIN (from other data pool) to process:</p>'
-    + '<form method="post" enctype="multipart/form-data">'
-    + '<p>File: <input type="file" name="cin" /></p>'
-    + '<p><input type="submit" value="Upload" /></p>'
-    + '</form>');
+    res.render('cin_confirm.ejs', {
+        title: 'CIN Upload Form',
+        upload: null
+    });
 });
 // CIN upload submit processing
 app.post('/cin', function(req, res) {
-    log('Request object: ');
-    log(req);
+    //log('Request object: ');
+    //log(req);
 
     var cinIn = req.files.cin.path;
     var cinOut = outboxDir + req.files.cin.name;
-    gdsn.readXmlFile(cinIn, function (err, xml) {
+    gdsn.readXmlFile(cinIn, function(err, xml) {
         if (err) {
             res.send(500, err);
             return;
         }
         var modXml = gdsn.processCinFromOtherDP(xml);
-        gdsn.writeXmlFile(cinOut, modXml);
-    });
-
-    res.render('cin_confirm.ejs', { 
-        title: 'CIN Upload Confirmation',
-        cin_filename: req.files.cin.name,
-        cin_filesize: (req.files.cin.size / 1024 | 0),
-        cin_filepath: req.files.cin.path,
-        cin_out: cinOut
+        gdsn.writeXmlFile(cinOut, modXml, function(err, result) {
+            if (err) {
+                log('Error: ' + err);
+                res.send(500, err);
+            }
+            else {
+                log('Result: ' + result);
+                res.render('cin_confirm.ejs', {
+                    upload: true,
+                    title: 'CIN Upload Confirmation',
+                    cin_filename: req.files.cin.name,
+                    cin_filesize: (req.files.cin.size / 1024 | 0),
+                    cin_filepath: req.files.cin.path,
+                    cin_out: cinOut
+                });
+            }
+        });
     });
 });
 
 // admin ui api route and default auth response test
 app.get('/admin/data.json', function(req, res) {
-    res.json({ authmask: "2097151", success: "true" });
+    res.json({
+        authmask: "2097151",
+        success: "true"
+    });
 });
 
-// ejs test with function factory (closure) approach
+// default snoop home page
 app.get('/', getSnoopHandler());
 
 function getSnoopHandler(count) {
-  count = count || 0;
-  return function(req, res) {
-    res.cookie('test_response_cookie', 'some cookie data, count ' + count++);
-    req.session.count = count;
-    req.session.timestamp = new Date().getTime();
-    res.contentType('text/html');
-    res.render('snoop.ejs', {
-      title: "Node HTTP Snoop",
-      req: req,
-      res: res
-    });
-  }
+    count = count || 0;
+    return function(req, res) {
+        res.cookie('test_response_cookie', 'some cookie data, count ' + count++);
+        req.session.count = count;
+        req.session.timestamp = new Date().getTime();
+        res.contentType('text/html');
+        res.render('snoop.ejs', {
+            title: "Node HTTP Snoop",
+            req: req,
+            res: res
+        });
+    }
 }
 
-app.listen(app.get('port'));
+app.listen(app.get('port'), process.env.IP);
 console.log("Express GDSN server listening on port " + app.get('port'));
 
 // Proof-of-concept: Inbox filesystem watcher
@@ -101,4 +110,3 @@ fs.watch(
     }
 );
 */
-
