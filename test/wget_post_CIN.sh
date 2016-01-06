2711bf16c0c521b21e193c6549e031282d0ea913
theoxylo <theoxylo@yahoo.com>
2016-01-06 13:00:17 -0800

    using message sender to determine GR

--- test/wget_post_CIN.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Go to this script's directory
-cd `dirname "$0"`
-
-wget --header="Content-type: multipart/form-data boundary=FILEUPLOAD" --post-file CIN_samples/CIN_IN_1242342_20019320017753.xml http://test:testAdmin@localhost:8080/cs_api/1.0/msg --auth-no-challenge
